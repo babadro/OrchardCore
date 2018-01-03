d610628ddcf26f248026db2afd85fc89fbc21634
Andrey Petrovich <baba_dro@mail.ru>
2018-01-03 13:04:08 +0300

    Display driver in process

---
+++ src/OrchardCore.Cms.Web/Modules/CustomModule/Drivers/ProductPartDisplayDriver.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using CustomModule.Models;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
+
+namespace CustomModule.Drivers
+{
+    public class ProductPartDisplayDriver : ContentPartDisplayDriver<ProductPart>
+    {
+        protected new string Prefix => "Product";
+
+        public override IDisplayResult Edit(ProductPart part)
+        {
+            return Shape("Parts_Product_Edit", part);
+        }
+
+        /*
+        public override IDisplayResult Display(TitlePart titlePart)
+        {
+            return Shape<TitlePartViewModel>("TitlePart", model =>
+            {
+                model.Title = titlePart.Title;
+                model.TitlePart = titlePart;
+            })
+            .Location("Detail", "Header:5")
+            .Location("Summary", "Header:5");
+        }
+
+        public override IDisplayResult Edit(TitlePart titlePart)
+        {
+            return Shape<TitlePartViewModel>("TitlePart_Edit", model =>
+            {
+                model.Title = titlePart.Title;
+                model.TitlePart = titlePart;
+
+                return Task.CompletedTask;
+            });
+        }
+
+        public override async Task<IDisplayResult> UpdateAsync(TitlePart model, IUpdateModel updater)
+        {
+            await updater.TryUpdateModelAsync(model, Prefix, t => t.Title);
+
+            return Edit(model);
+        }
+         */
+    }
+}
