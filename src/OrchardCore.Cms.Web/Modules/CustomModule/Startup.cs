d0543097ac462f57dc268dc5af50e13ab1734a59
Andrey.Petrovich <baba_dro@mail.ru>
2018-01-15 08:54:07 +0300

    flow go to ProductPartDisplayDriver

--- src/OrchardCore.Cms.Web/Modules/CustomModule/Startup.cs
+++ src/OrchardCore.Cms.Web/Modules/CustomModule/Startup.cs
@@ -1,6 +1,9 @@
+using CustomModule.Drivers;
 using CustomModule.Models;
+using Fluid;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.Data.Migration;
 using OrchardCore.Modules;
 
@@ -10,11 +13,12 @@ namespace CustomModule
     {
         static Startup()
         {
-
+            TemplateContext.GlobalMemberAccessStrategy.Register<ProductPart>();
         }
 
         public override void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IContentPartDisplayDriver, ProductPartDisplayDriver>();
             services.AddSingleton<ContentPart, ProductPart>();
             services.AddScoped<IDataMigration, Migrations>();
         }
